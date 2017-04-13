36349236c2b5dd14fe5fea72848b4c9b7f7ec472
RickardSjogren <r.sjogren89@gmail.com>
2017-04-13 09:37:14 +0200

    Added setup.py

--- README.rst
+++ README.rst
@@ -1,4 +1,5 @@
-# VP-Tree
+VP-Tree
+=======
 
 This package contains an implementation of [a vantage-point tree](https://en.wikipedia.org/wiki/Vantage-point_tree)
 data structure.
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/use/bin/env python
+
+from distutils.core import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        contents = f.read()
+    return contents
+
+setup(
+    name='vptree',
+    version='1.0',
+    author='Rickard Sjögren',
+    author_email='r.sjogren89@gmail.com',
+    license='MIT',
+    url='https://github.com/RickardSjogren/vptree',
+    description=('A package implementing a vantage-point data structure, for '
+                 'efficient nearest neighbor searching.'),
+    long_description=readme(),
+    py_modules=['vptree'],
+    test_suite='test',
+    keywords='python machine learning search',
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
+)
